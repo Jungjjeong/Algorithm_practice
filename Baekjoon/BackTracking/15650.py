3eb196d941616f97a500343a54156929f59f5d68
Jungjjeong <sky990106@dgu.ac.kr>
2022-01-17 21:59:55 +0900

    15650 solution

---
+++ Baekjoon/BackTracking/15650.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split(' '))
+
+checked_list = [0] * n
+used_list = [0] * n
+num_arr = []
+
+
+def dfs(index):
+    if index == m:
+        print(*num_arr)
+        return
+
+    for i in range(n):
+        if index >= 1 and num_arr[index-1] >= i+1:
+            continue
+        if checked_list[i] == 0:
+            checked_list[i] = 1
+            num_arr.append(i+1)
+            dfs(index + 1)
+            num_arr.remove(i+1)
+            checked_list[i] = 0
+
+
+dfs(0)
