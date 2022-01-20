1041e6bb3e67050b6f377bd69b5bbc6f545da844
Jungjjeong <sky990106@dgu.ac.kr>
2022-01-20 20:50:43 +0900

    14889 solution (not perfect)

--- Baekjoon/BackTracking/14889.py
+++ Baekjoon/BackTracking/14889.py
@@ -7,7 +7,7 @@ for i in range(n):
 team_arr = []
 team = []
 checked_list = [0] * n
-min_result = 0
+min_result = 10000
 
 
 def dfs(index):
@@ -34,8 +34,28 @@ def dfs(index):
 
 dfs(0)
 
-# for t in team:
-#     for i in team[0: n/2]:
-#         print(i)
+for i in range(len(team)):
+    t = team[i]
+    print(t)
+    sum_1 = 0
+    for j in range(n//2):
+        # 앞의 팀
+        person = t[j]
+        for k in t:
+            sum_1 += s_arr[person-1][k-1]
+    print(sum_1)
+
+    sum_2 = 0
+    for j in range(n//2):
+        person = t[n//2+j]
+        for k in t:
+            sum_2 += s_arr[person-1][k-1]
+    print(sum_2)
+
+    print(sum_1 - sum_2)
+    min_result = min(min_result, abs(sum_1 - sum_2))
+    print(min_result)
+
 
 print(team)
+print(min_result)
