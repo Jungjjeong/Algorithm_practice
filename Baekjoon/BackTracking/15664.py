6743e84b65cd885b27e80a31eab10dcbe071e949
Jungjjeong <sky990106@dgu.ac.kr>
2022-01-25 17:48:22 +0900

    15664 solution

---
+++ Baekjoon/BackTracking/15664.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split(' '))
+
+num = list(map(int, input().split(' ')))
+num.sort()
+
+num_arr = []
+checked_list = [0] * n
+
+
+def dfs(index):
+    if index == m:
+        print(*num_arr)
+        return
+
+    before_num = 0
+
+    for i in range(n):
+        if index >= 1 and num_arr[index-1] > num[i]:
+            continue
+
+        if checked_list[i] == 0 and num[i] != before_num:
+            checked_list[i] = 1
+            num_arr.append(num[i])
+            before_num = num[i]
+            dfs(index + 1)
+            checked_list[i] = 0
+            num_arr.pop()
+
+
+dfs(0)
