7838770b63fe469039f3a6d715239ca4a39ed66e
Jungjjeong <sky990106@dgu.ac.kr>
2022-01-30 18:34:10 +0900

    14888 solution

---
+++ Baekjoon/BackTracking/14888.py
@@ -0,0 +1,43 @@
+from itertools import permutations as per
+from itertools import combinations as com
+
+
+n = int(input())
+n_list = list(map(int, input().split(' ')))
+operator_input_list = list(map(int, input().split(' ')))
+
+operator_list = []
+
+for i in range(4):
+    if operator_input_list[i] > 0:
+        for _ in range(operator_input_list[i]):
+            operator_list.append(i)
+
+com_n_list = list(com(n_list, n))
+per_o_list = list(per(operator_list, n-1))
+
+max = float('-inf')
+min = float('inf')
+
+for cn in com_n_list:
+    for po in per_o_list:
+        temp = cn[0]
+        for i in range(n-1):
+            if po[i] == 0:
+                temp += cn[i+1]
+            elif po[i] == 1:
+                temp -= cn[i+1]
+            elif po[i] == 2:
+                temp *= cn[i+1]
+            elif po[i] == 3:
+                if temp < 0:
+                    temp = -(abs(temp) // cn[i+1])
+                else:
+                    temp //= cn[i+1]
+        if temp > max:
+            max = temp
+        if temp < min:
+            min = temp
+
+print(max)
+print(min)
