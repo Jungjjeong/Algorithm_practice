985c5f29978c21e9a9ffa304bb238f6113a7db03
Jungjjeong <sky990106@dgu.ac.kr>
2022-03-15 21:20:45 +0900

    2638 solution

---
+++ Baekjoon/simulation/2638.py
@@ -0,0 +1,61 @@
+from collections import deque
+
+
+h, w = map(int, input().split(' '))
+field = []
+visit = [[0 for _ in range(w)] for _ in range(h)]
+cheese = 0
+
+for _ in range(h):
+	f_list = list(map(int, input().split(' ')))
+	for f in f_list:
+		if f == 1:
+			cheese += 1
+	field.append(f_list)
+
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
+
+def solution(y, x):
+	q = deque()
+	q.append((y, x))
+	visit[y][x] = 1
+	
+	while q:
+		y, x = q.popleft()
+		for i in range(4):
+			ny = y + dy[i]
+			nx = x + dx[i]
+
+			if 0 <= ny < h and 0<= nx < w and visit[ny][nx] == 0:
+				if field[ny][nx] >= 1:
+					field[ny][nx] += 1
+				elif field[ny][nx] == 0:
+					q.append((ny, nx))
+					visit[ny][nx] = 1
+
+count = 0
+
+if cheese == 0:
+	print(0)
+	quit(0)
+
+while True:
+	isTrue = False
+	solution(0, 0)
+
+	for i in range(h):
+		for j in range(w):
+			if field[i][j] > 2:
+				field[i][j] = 0
+			if 0 < field[i][j] <= 2:
+				field[i][j] = 1
+				isTrue = True
+
+	count += 1
+	
+	if isTrue == False:
+		print(count)
+		break
+
+	visit = [[0 for _ in range(w)] for _ in range(h)]
