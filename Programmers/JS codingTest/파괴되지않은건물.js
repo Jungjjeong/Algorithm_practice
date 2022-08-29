98394b09ae696eff99634ca9b68ac3e365ca58b4
Jungjjeong <sky990106@dgu.ac.kr>
2022-08-29 14:38:00 +0900

    solution immo algorithm

---
+++ Programmers/JS codingTest/파괴되지않은건물.js
@@ -0,0 +1,45 @@
+function solution(board, skill) {
+  let answer = 0;
+  const row = board.length;
+  const col = board[0].length;
+  let imosArr = Array.from({ length: row + 1 }, () =>
+    new Array(col + 1).fill(0)
+  );
+
+  for (let i = 0; i < skill.length; i += 1) {
+    const [type, r1, c1, r2, c2, degree] = skill[i];
+
+    imosArr[r1][c1] += type === 1 ? -degree : degree;
+    imosArr[r1][c2 + 1] += type === 1 ? degree : -degree;
+    imosArr[r2 + 1][c1] += type === 1 ? degree : -degree;
+    imosArr[r2 + 1][c2 + 1] += type === 1 ? -degree : degree;
+  }
+
+  for (let i = 0; i < row; i += 1) {
+    let sum = 0;
+    for (let j = 0; j < col; j += 1) {
+      sum += imosArr[i][j];
+      imosArr[i][j] = sum;
+    }
+  }
+
+  for (let i = 0; i < col; i += 1) {
+    let sum = 0;
+    for (let j = 0; j < row; j += 1) {
+      sum += imosArr[j][i];
+      imosArr[j][i] = sum;
+    }
+  }
+
+  for (let i = 0; i < row; i += 1) {
+    for (let j = 0; j < col; j += 1) {
+      board[i][j] += imosArr[i][j];
+
+      if (board[i][j] > 0) {
+        answer++;
+      }
+    }
+  }
+
+  return answer;
+}
